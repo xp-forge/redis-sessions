b68496184bf16ada1a61b3e05acb603c73e7c998
Timm Friebe <thekid@thekid.de>
2021-10-24 10:32:34 +0200

    Convert tests to baseless Assert DSL

--- src/test/php/web/session/redis/unittest/RedisTest.class.php
+++ src/test/php/web/session/redis/unittest/RedisTest.class.php
@@ -1,10 +1,10 @@
 <?php namespace web\session\redis\unittest;
 
 use io\redis\RedisProtocol;
-use unittest\{Expect, Test, TestCase};
+use unittest\{Assert, Expect, Test, TestCase};
 use web\session\{ISession, Redis, SessionInvalid};
 
-class RedisTest extends TestCase {
+class RedisTest {
 
   #[Test]
   public function create_session() {
@@ -13,7 +13,7 @@ class RedisTest extends TestCase {
 
     $session= $fixture->create();
     $id= $session->id();
-    $this->assertEquals(
+    Assert::equals(
       sprintf(
         "*4\r\n\$4\r\nHSET\r\n\$%1\$d\r\nsession:%2\$s\r\n\$1\r\n_\r\n\$5\r\n.keep\r\n".
         "*3\r\n\$6\r\nEXPIRE\r\n\$%1\$d\r\nsession:%2\$s\r\n\$5\r\n%3\$d\r\n",
@@ -31,8 +31,8 @@ class RedisTest extends TestCase {
     $fixture= new Redis(new RedisProtocol($io));
 
     $session= $fixture->open('test');
-    $this->assertEquals("*2\r\n\$3\r\nTTL\r\n\$12\r\nsession:test\r\n", $io->out);
-    $this->assertInstanceOf(ISession::class, $session);
+    Assert::equals("*2\r\n\$3\r\nTTL\r\n\$12\r\nsession:test\r\n", $io->out);
+    Assert::instance(ISession::class, $session);
   }
 
   #[Test]
@@ -41,8 +41,8 @@ class RedisTest extends TestCase {
     $fixture= new Redis(new RedisProtocol($io));
 
     $session= $fixture->open('test');
-    $this->assertEquals("*2\r\n\$3\r\nTTL\r\n\$12\r\nsession:test\r\n", $io->out);
-    $this->assertNull($session);
+    Assert::equals("*2\r\n\$3\r\nTTL\r\n\$12\r\nsession:test\r\n", $io->out);
+    Assert::null($session);
   }
 
   #[Test]
@@ -50,7 +50,7 @@ class RedisTest extends TestCase {
     $io= new Channel(":86300\r\n\$7\r\n\"value\"\r\n");
     $fixture= new Redis(new RedisProtocol($io));
 
-    $this->assertEquals('value', $fixture->open('test')->value('value'));
+    Assert::equals('value', $fixture->open('test')->value('value'));
   }
 
   #[Test, Expect(SessionInvalid::class)]
